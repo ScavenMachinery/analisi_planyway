f4fc0e5de27472a9c489756c4e899bf16eaa15d0
ScavenMachinery <131487114+ScavenMachinery@users.noreply.github.com>
2024-03-24 18:07:05 +0100

    Update analisiplanyway.py

--- analisiplanyway.py
+++ analisiplanyway.py
@@ -76,7 +76,7 @@ if password_input:
 
         selected_month = st.sidebar.date_input(
             "seleziona il periodo",
-            jan_1, datetime.date(this_year,1,7),
+            (jan_1, datetime.date(this_year,1,7)),
             jan_1,
             dec_31,
             format="YYYY.MM.DD",)
